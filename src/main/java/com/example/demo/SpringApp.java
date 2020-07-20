1425c01f2bf51efa48d78904571cab6ab9b3d6c3
bbarnhi <66078486+bbarnhi@users.noreply.github.com>
2020-07-20 14:57:27 -0400

    Path Variables

--- src/main/java/com/example/demo/SpringApp.java
+++ src/main/java/com/example/demo/SpringApp.java
@@ -4,47 +4,94 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.Map;
 
 @RestController
 public class SpringApp {
 
-    @GetMapping("/math/pi")
     public String piGet(){
         return "3.141592653589793";
     }
 
-    @GetMapping("/math/calculate")
-    public String getDirect(WebRequest webRequest){
-        String operation = webRequest.getParameter("operation");
-        String value1 = webRequest.getParameter("value1");
-        String value2 = webRequest.getParameter("value2");
-        Map<String, String[]> params = webRequest.getParameterMap();
-        {
-            //System.out.println(params[0]);
-            System.out.println(operation+value1+value2);
+    @GetMapping("/hwl/{height}/{width}/{length}")
+    public String getMapParams(@PathVariable Map<String, String> querystring){
+        int lwh = 1;
+        for (String i : querystring.keySet()) {
+            lwh *= Integer.valueOf(querystring.get(i));
 
+        }
+        return String.valueOf(lwh);
+    }
+
+//////////////////////////////  First attempt using Mapping for number values
+//    @GetMapping("/math/calculate")
+//    public String getDirect(WebRequest webRequest){
+//        String operation = webRequest.getParameter("operation");
+//        String value1 = webRequest.getParameter("value1");
+//        String value2 = webRequest.getParameter("value2");
+//        Map<String, String[]> params = webRequest.getParameterMap();
+//
+//        {
+//            //System.out.println(params[0]);
+//            System.out.println(operation+value1+value2);
+//
+//        if (operation.equals("add") || operation == null){
+//            System.out.println("In the add function");
+//            return ("Addition Operation: "+(Integer.valueOf(value1) + Integer.valueOf(value2)));
+//        }
+//        else if (operation.equals("subtract")){
+//            return ("Substration Operation: "+(Integer.valueOf(value1) - Integer.valueOf(value2)));
+//        }
+//        else if (operation.equals("multiply")){
+//            return ("Multiply Operation: "+(Integer.valueOf(value1) * Integer.valueOf(value2)));
+//        }
+//        else if (operation.equals("divide")){
+//            return ("Divide Operation: "+(Integer.valueOf(value1) / Integer.valueOf(value2)));
+//        }
+//        else { return "No operation"; }
+//        }
+//    }
+//////////////////////////////////////////////
+
+/////////////////////////////////  Pulling 1 unique and a unknown number of nums from URL
+    @GetMapping("/math/calculate")
+    public String getMathOpts(
+                    @RequestParam (value="operation", defaultValue = "add") String operation,
+                    @RequestParam (value="n", required=true) Integer[] nums) {
+        Integer tempValue = 0;
         if (operation.equals("add") || operation == null){
             System.out.println("In the add function");
-            return ("Addition Operation: "+(Integer.valueOf(value1) + Integer.valueOf(value2)));
+            for (int i=0;i<nums.length;i++){
+                tempValue += nums[i]; }
+            return ("Addition Operation: "+ tempValue);
         }
         else if (operation.equals("subtract")){
-            return ("Substration Operation: "+(Integer.valueOf(value1) - Integer.valueOf(value2)));
+            for (int i=0;i<nums.length;i++){
+                tempValue -= nums[i]; }
+            return ("Substraction Operation: "+ tempValue);
         }
         else if (operation.equals("multiply")){
-            return ("Multiply Operation: "+(Integer.valueOf(value1) * Integer.valueOf(value2)));
+            for (int i=0;i<nums.length;i++){
+                tempValue *= nums[i]; }
+            return ("Multiplication Operation: "+ tempValue);
         }
         else if (operation.equals("divide")){
-            return ("Divide Operation: "+(Integer.valueOf(value1) / Integer.valueOf(value2)));
+            for (int i=0;i<nums.length;i++){
+                tempValue /= nums[i]; }
+            return ("Division Operation: "+ tempValue);
         }
-        else { return "No operation"; }
+        else { return "No operation";
         }
     }
 
+////// Not functional
 //    public List<T> getAll(WebRequest webRequest){
 //        Map<String, String[]> params = webRequest.getParameterMap();
 //        System.out.println( params );
 
+
+    //////////////////////////////  Getting 1 to 1 Value from URL
 //    @GetMapping("/math/calculate")
 //    public String getMathOpts(
 //        @RequestParam(value="operation", defaultValue = "add") String operation ,
